be552c6b8222c9bc50fa026ae7562be1776a366b
Luiz Parente <31785518+luizparente@users.noreply.github.com>
2017-10-15 14:52:32 -0400

    Update 001_Hello_World.cs

--- 001_Hello_World.cs
+++ 001_Hello_World.cs
@@ -9,9 +9,9 @@ namespace Basics
             // In the context of C#, "Console" is a class that, in simple very words, represent
             // the text stream being shown in a console application. In this course, the Console
             // will be used to both display outputs and gather inputs from the user.
-            // The WriteLine method, as the name suggests, inserts a line, displaying it to the user.
+            // The WriteLine method, as the name suggests, inserts a line, displaying it on to the screen.
             // It may contain text, numbers, or values that were previously stored in a variable.
-            // If the WriteLine method are being used to show simple text strings,
+            // If the WriteLine method is being used to show simple text strings,
             // double quotes "" must be used, and the text inside the parenthesis ("text") will be displayed.
             // The line below is your first C# console application. It displays the text  "Hello World" to the user.
             
