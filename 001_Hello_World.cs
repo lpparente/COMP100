67dd98fd8fd97b92b47fbac8531e9d2e42967b71
Luiz Parente <31785518+luizparente@users.noreply.github.com>
2017-10-15 21:06:51 -0400

    Update 001_Hello_World.cs

--- 001_Hello_World.cs
+++ 001_Hello_World.cs
@@ -1,4 +1,18 @@
-﻿using System;
+/*
+In the context of C#, "Console" is a class that is invoked always when there is a need to interact with the user
+via Console. As we are starting in a context of console applications, the Console will be used to both display 
+outputs and gather inputs from the user.
+
+The WriteLine method, as the name suggests, inserts a line, displaying it on to the screen.
+It may contain text, numbers, or values that were previously stored in a variable.
+If the WriteLine method is being used to show simple text strings, double quotes "" must be used, and the text 
+inside the parenthesis ("text") will be displayed. The code below is your first C# console application. 
+It displays the text "Hello World!" to the user.
+
+You can copy and paste the lines below into your IDE of choice and see how this code works.
+*/
+
+using System;
 
 namespace Basics
 {
@@ -6,17 +20,6 @@ namespace Basics
     {
         public static void Main(string[] args)
         {
-            // In the context of C#, "Console" is a class that, in simple very words, represent
-            // the text stream being shown in a console application. In this course, the Console
-            // will be used to both display outputs and gather inputs from the user.
-            // The WriteLine method, as the name suggests, inserts a line, displaying it on to the screen.
-            // It may contain text, numbers, or values that were previously stored in a variable.
-            // If the WriteLine method is being used to show simple text strings,
-            // double quotes "" must be used, and the text inside the parenthesis ("text") will be displayed.
-            // The line below is your first C# console application. It displays the text  "Hello World" to the user.
-            
-            // You can copy and paste the line 20 into your IDE of choice and see how this code works.
-
             Console.WriteLine("Hello World!");
         }
     }
