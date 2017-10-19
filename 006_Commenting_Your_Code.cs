6310b3c637938462ea6b185349b2a0904c3974b8
Luiz Parente <31785518+luizparente@users.noreply.github.com>
2017-10-18 22:44:11 -0400

    Add files via upload

---
+++ 006_Commenting_Your_Code.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Variables
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            /*
+            Have you ever noticed how there are lots of explanations and instructions in every file of this course?
+            This is dont by adding comments into the executable code. There are two ways to do that:
+            */
+
+            // You can type two slashes //, and then type whatever note or reminder after that. Just like I did in this line.
+
+            /*
+            Or, in case a block of comments with more than one line is needed, you can do it like this!
+            Then you can have multiple lines to write anything that is relevant to your code.
+            */
+
+            /*
+            This is called DOCUMENTATION. It is very important in professional coding, as programmers usually work in teams.
+            Not everyone is able to understand someone else's code in just a glance. So it is a good practice to
+            comment and document as much as possible, so that the code can be easily understood by your colleagues.
+
+            Also, your comments may be of great help for yourself in the future! If you ever have to go back to an
+            application you coded one or two years ago, chances are you will not be able to remember everything that passed
+            through your mind when you first worked in that project. Helpful comment lines can save you hours of screen
+            staring and trying to figure out what you did.
+
+            This is one of the most important tools. Comments are available in every programming language.
+
+            Be wise. Use it!
+            */
+        }
+    }
+}
