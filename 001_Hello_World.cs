e506d81b2e3019e30fe55dc6a309ef0a56a93bca
Luiz Parente <31785518+luizparente@users.noreply.github.com>
2017-10-20 13:28:11 -0400

    Update 001_Hello_World.cs

--- 001_Hello_World.cs
+++ 001_Hello_World.cs
@@ -1,15 +1,17 @@
 /*
-In C#, "Console" is a class that is invoked always when there is a need to interact with the user
-via Console. As we are starting in a context of console applications, the Console will be used to both display 
-outputs and gather inputs from the user.
+As we are starting this course in a simple context, the Console will be used to both display 
+outputs and gather inputs from the user. C#, and also most programming languages, have vast resources dedicated
+to interacting with the user via Console.
+
+"Hello World" is a classic introduction to programming languages. Due to its simplicity, it is easy to introduce
+basic concepts to the student, such as displaying information on the screen.
 
 The WriteLine method, as the name suggests, inserts a line, displaying it on to the screen.
-It may contain text, numbers, or values that were previously stored in a variable.
-If the WriteLine method is being used to show simple text strings, double quotes "" must be used, and the text 
-inside the parenthesis ("text") will be displayed. The code below is your first C# console application. 
-It displays the text "Hello World!" to the user.
+It may contain text or numbers. As we want to display the message "Hello World!", which is a text string,
+double quotes "" must be used.
 
-You can copy and paste the lines below into your IDE of choice and see how this code works.
+The code below is your first C# console application. You can copy and paste the lines below into your IDE of 
+choice and see how this code works.
 */
 
 using System;
