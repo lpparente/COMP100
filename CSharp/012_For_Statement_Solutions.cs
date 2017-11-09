8d4d7de54ccee0476dc2a297ad52c076d1c99fd3
Luiz Parente <31785518+luizparente@users.noreply.github.com>
2017-11-08 21:57:58 -0500

    Add files via upload

---
+++ CSharp/012_For_Statement_Solutions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace CSharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            /*
+            EXPLORATION EXERCISES - Solutions:
+
+            1) Write a program that prints all multiples of 7 from 0 to 100.
+            */
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 7 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            /*
+            2) Write a program that displays the sum of all numbers from 0 to 100.
+            */
+
+            int sum = 0;
+
+            for (int i = 0; i <= 100; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine(sum);
+
+            /*
+            3) Write a program that prompts the user for a number. The program must return the factorial of this number.
+            Remark: Factorial of 5 = 5! = 5 * 4 * 3 * 2 * 1.
+            */
+
+            Console.WriteLine("Please input an integer number: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            int factorial = 1;
+
+            for (int i = 1; i <= userInput; i++)
+            {
+                factorial = factorial * i;
+            }
+
+            Console.WriteLine(factorial);
+        }
+    }
+}
